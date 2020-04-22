8a03ad865d67274d25cc676a3a7fee28701ea9f0
wnuke <willem@leit.so>
2020-04-22 11:38:57 +0200

    made it output a link instead of json

--- forge_help.sh
+++ forge_help.sh
@@ -9,13 +9,14 @@ else
 fi
 
 
-if [ "$1" == "logs" ] || [ "$1" == "log" ]; then
+if [[ "$1" == "log" ]]; then
 	if [[ -d "$mc_folder/logs" ]]; then
 		if [[ -f "$mc_folder/logs/latest.log" ]]; then
 			out=$(curl -X POST https://hastebin.com/documents -T "$mc_folder/logs/latest.log")
 			echo
-			echo $out
-			echo Send this Line to the support
+			key=`echo  $out | sed 's/{"key":"//;s/"}//'`
+            echo "https://hastebin.com/$key"
+			echo Send this link to the support
 			echo
 		else
 			echo Missing latest.log File
@@ -23,22 +24,23 @@ if [ "$1" == "logs" ] || [ "$1" == "log" ]; then
 	else
 		echo Missing minecraft folder
 	fi
-elif [ "$1" == "mods" ] || [ "$1" == "mod" ]; then
+elif [[ "$1" == "mods" ]]; then
 	if [[ -d "$mc_folder/mods" ]]; then
-        if [[ $use_tree == true ]]; then
+        if [ $use_tree == true ]; then
             which tree > /dev/null
 		    if [[ $? -ne 0 ]]; then
 		    	echo missing tree Please Install
 		    	sudo apt install tree
 		    fi
-		    out=$(tree "$mc_folder/mods")
+		    out=$(find "$mc_folder/mods")
         else
 		    out=$(find "$mc_folder/mods")
         fi
 		out=$(curl -X POST https://hastebin.com/documents -d "$out")
 		echo
-		echo $out
-		echo Send this Line to the support 
+        key=`echo  $out | sed 's/{"key":"//;s/"}//'`
+        echo "https://hastebin.com/$key"
+        echo Send this link to the support
         echo
 	else
 		echo Missing mods Folder
